5c291e78bca547dcc80bbdfd5f3f9afbb3a6cf8d
kennyChukwuebuka <kenkinado@gmail>
2023-11-07 23:13:29 +0100

    updated the basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,25 @@ from datetime import datetime
 
 class BaseModel:
     """A BaseModel class that defines all common attribute/methods"""
-    def __init__(self):
-        """initialize the BaseModel"""
+    def __init__(self, *args, **kwargs):
+        """initialize the BaseModel
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        Args:
+            *args: argument constructor (Unused)
+            **kwargs: argument constructor
+        """
+        t_formart = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, t_formart))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """Updates the public instance attribute"""

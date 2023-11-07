a610aa033a63d4be16e03bc6d0abac52e493b058
kennyChukwuebuka <kenkinado@gmail>
2023-11-07 21:13:49 +0100

    AirBnB

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""class BaseModel"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """A BaseModel class that defines all common attribute/methods"""
+    def __init__(self):
+        """initialize the BaseModel"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def save(self):
+        """Updates the public instance attribute"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)

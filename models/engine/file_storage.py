da4f2c30a9c697d8f89002885f475f0423c35acb
kennyChukwuebuka <kenkinado@gmail>
2023-11-08 12:53:54 +0100

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,11 +11,10 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-
     def all(self):
         """Returns the dictionaries object"""
         return FileStorage.__objects
-    
+
     def new(self, obj):
         """Sets in the obj with key"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
@@ -28,7 +27,7 @@ class FileStorage:
             obj_dict[key] = value.to_dict()
         with open(FileStorage.__file_path, 'w', encoding='utf=8') as file:
             json.dump(obj_dict, file)
-    
+
     def reload(self):
         """Deserialize the JSON file to object if it exit"""
         try:

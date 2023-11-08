9d74983f59682d1bed54e587cb1c121fd16cbae3
kennyChukwuebuka <kenkinado@gmail>
2023-11-08 12:49:11 +0100

    file storage added

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """class BaseModel"""
 
+import models
 import uuid
 from datetime import datetime
 
@@ -26,10 +27,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def save(self):
         """Updates the public instance attribute"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()

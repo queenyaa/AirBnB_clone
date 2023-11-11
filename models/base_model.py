6238be0d3c1e68616e8496fc29a347d362289d3c
kennyChukwuebuka <kenkinado@gmail>
2023-11-11 15:58:27 +0100

    updated basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """class BaseModel"""
 
 import models
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 
@@ -24,7 +24,7 @@ class BaseModel:
                     else:
                         setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)

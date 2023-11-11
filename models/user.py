cda4896acd1674f411f190641ba8967e2eaf3a94
kennyChukwuebuka <kenkinado@gmail>
2023-11-11 15:42:31 +0100

    pycodestyle

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Class user that inherit from BaseModel"""
 
+
 from models.base_model import BaseModel
+
 
 class User(BaseModel):
     """User class inherit from BaseModel"""
